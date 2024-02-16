e67f80408afe2a4a3e86ba6b88cafeeaf7fa0543
ngoquy12 <ngoq753429@gmail.com>
2024-02-16 18:16:34 +0700

    update file

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Lấy dữ liệu trên local
+const userLogin = JSON.parse(localStorage.getItem("userLogin"));
+
+const userLoginElement = document.getElementById("userLogin");
+
+if (userLogin) {
+  // Hiển thị tên của user đang đăng nhập lên header
+  userLoginElement.innerHTML = userLogin.userName;
+} else {
+  userLoginElement.innerHTML = "";
+}
